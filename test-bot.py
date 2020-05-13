b930c6e8b848da879a0133ac8406a4925b7f2e9e
Bùi Việt Dũng <dvbui@wisc.edu>
2020-05-13 04:44:16 -0400

    add codename

--- test-bot.py
+++ test-bot.py
@@ -8,7 +8,10 @@ async def on_message(message):
 	if (message.author == client.user):
 		return
 	message.content = message.content.lower()
-	if (message.content.startswith("hello")):
+	args = message.content.split(' ')
+	if (args[0]!="olym"):
+	 	return
+	if (args[1].startswith("hello")):
 		await message.channel.send("Hello, I am a test bot.")
 
 f = open("secret.txt","r")
