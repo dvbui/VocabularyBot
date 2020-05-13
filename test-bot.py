26d0743fd5c312a9d4f8d0f1bd89d5add0b1aaf7
Bùi Việt Dũng <dvbui@wisc.edu>
2020-05-13 05:27:32 -0400

    fix err

--- test-bot.py
+++ test-bot.py
@@ -14,7 +14,8 @@ wordFile.close()
 async def on_message(message):
 	if (message.author == client.user):
 		return
-	if (message.author.bot) return;
+	if (message.author.bot):
+		return;
 
 	message.content = message.content.lower()
 	args = message.content.split(' ')
