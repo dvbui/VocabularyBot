f9c71154039ab83468e68d35a2b75705ae45d808
Bùi Việt Dũng <dvbui@wisc.edu>
2020-05-14 06:31:58 -0400

    better formatting

--- messenger.py
+++ messenger.py
@@ -10,10 +10,14 @@ def register_message():
 
 def question_message(question, index="", type_of_word="", number_of_character=""):
     mess = "```\n"
+    mess += "\n"
     mess += "Clue {} ({},{} characters): {}\n".format(index, type_of_word, number_of_character, question)
+    mess += "\n"
     mess += "You have 15 seconds to answer this clue.\n"
     mess += "Type your answer (and only your answer) to answer your question.\n"
     mess += "If you haven't register, type \"olym register\" to register.\n"
+    mess += "\n"
     mess += "To solve this block, type \"olym solve [answer]\"\n"
+    mess += "\n"
     mess += "```\n"
     return mess
