d892f7a33d7a4bfd41fbb7258d54f4b11ca0a4ca
Bùi Việt Dũng <dvbui@wisc.edu>
2020-05-22 21:30:47 -0400

    try again

--- wordnet-bot.py
+++ wordnet-bot.py
@@ -28,7 +28,7 @@ def create_credential_file():
         "type": os.environ["FIREBASE_TYPE"],
         "project_id": os.environ["FIREBASE_PROJECT_ID"],
         "private_key_id": os.environ["FIREBASE_PRIVATE_KEY_ID"],
-        "private_key": os.environ["FIREBASE_PRIVATE_KEY"].replace("\\\\", "\\"),
+        "private_key": os.environ["FIREBASE_PRIVATE_KEY"].replace("\\n", "\n"),
         "client_email": os.environ["FIREBASE_CLIENT_EMAIL"],
         "client_id": os.environ["FIREBASE_CLIENT_ID"],
         "auth_uri": os.environ["FIREBASE_AUTH_URI"],
